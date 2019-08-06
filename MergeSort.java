9ebf3e20ddf45ce198ffcac7bd203b80050b9a7b
reidsm <45023406+reidsm@users.noreply.github.com>
2019-08-06 15:15:40 -0700

    Add files via upload

---
+++ MergeSort.java
@@ -0,0 +1,95 @@
+import java.util.*;
+public class MergeSort {
+    // Driver method
+    public static void main(String args[]) {
+        int[] lst = {12, 11, 13, 5, 6, 7};
+
+        System.out.println("Given Array");
+        System.out.println(Arrays.toString(lst));
+
+        sort(lst, 0, lst.length-1);
+
+        System.out.println("\nSorted array");
+        System.out.println(Arrays.toString(lst));
+    }
+
+    // Main function that sorts arr[l..r] using
+    // merge()
+
+    public static void sort(int[] lst, int firstIndex, int lastIndex) {
+        if (firstIndex < lastIndex) {
+            // Find the middle point
+            int m = (firstIndex+lastIndex)/2;
+
+            // Sort first and second halves
+            sort(lst, firstIndex, m);
+            sort(lst, m+1, lastIndex);
+
+            // Merge the sorted halves
+            merge(lst, firstIndex, m, lastIndex);
+        }
+    }
+
+    // Merges two subarrays of arr[].
+    // First subarray is arr[l..m]
+    // Second subarray is arr[m+1..r]
+
+    public static void merge(int [] lst, int firstIndex, int m, int lastIndex) {
+        // Find sizes of two subarrays to be merged
+        int leftLength = m - firstIndex + 1;
+        int rightLength = lastIndex - m;
+
+        /* Create temp arrays */
+        int L[] = new int [leftLength];
+        int R[] = new int [rightLength];
+
+        /*Copy data to temp arrays*/
+        for (int i=0; i<leftLength; ++i) {
+            L[i] = lst[firstIndex + i];
+        }
+        for (int j=0; j<rightLength; ++j) {
+            R[j] = lst[m + 1 + j];
+        }
+
+
+        /* Merge the temp arrays */
+
+        // Initial indexes of first and second subarrays
+        int i = 0, j = 0;
+
+        // Initial index of merged subarry array
+        int newIndex = firstIndex;
+
+        while (i < leftLength && j < rightLength) {
+            if (L[i] <= R[j]) {
+                lst[newIndex] = L[i];
+                i++;
+            }
+            else {
+                lst[newIndex] = R[j];
+                j++;
+            }
+            newIndex++;
+        }
+
+        /* Copy remaining elements of L[] if any */
+        while (i < leftLength) {
+            lst[newIndex] = L[i];
+            i++;
+            newIndex++;
+        }
+
+        /* Copy remaining elements of R[] if any */
+        while (j < rightLength) {
+            lst[newIndex] = R[j];
+            j++;
+            newIndex++;
+        }
+    }
+}
+/* This code is contributed by Rajat Mishra https://www.geeksforgeeks.org/merge-sort/ */
+
+
+
+
+
